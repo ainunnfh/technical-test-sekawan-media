2110af59e41da2ae9546ebaa322a0ac90d89eb21
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-07-17 23:55:53 +0700

    ⭐new (schema): create models schema

---
+++ prisma/prisma/migrations/20240717165450_create_schema/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE `Users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `roleId` INTEGER NULL,
+
+    UNIQUE INDEX `Users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Roles` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Tickets` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `details` VARCHAR(191) NOT NULL,
+    `customer_name` VARCHAR(191) NOT NULL,
+    `date` VARCHAR(191) NOT NULL,
+    `priority` VARCHAR(191) NOT NULL,
+    `category` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Users` ADD CONSTRAINT `Users_roleId_fkey` FOREIGN KEY (`roleId`) REFERENCES `Roles`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
