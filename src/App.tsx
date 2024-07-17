a481ae76dc166e455309409070f9d40cdbc1d439
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-07-18 06:59:53 +0700

    🆙update (app): create routing for ticket page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Login from "./components/Login";
-function App() {
-  return <Login />;
+import Sidebar from "./components/Sidebar";
+import { userDummy } from "./constans/DummyData";
+
+interface IUser {
+  name: string;
+  email: string;
+  role: string;
+}
+
+const userAdmin = userDummy.find((user) => user.role === "Admin");
+console.log(userAdmin);
+
+function App({ email }: IUser) {
+  return <Sidebar email={email} />;
 }
 
 export default App;
---
+++ src/pages/TicketPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TableTicket from "../components/TableTicket";
+
+const TicketPage = () => {
+  return <TableTicket />;
+};
+
+export default TicketPage;
