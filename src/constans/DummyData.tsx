130f1e3db14decab6ab63eff565fd08a9e21597d
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-07-17 21:39:14 +0700

    ⭐(constants): create dummy data

---
+++ src/constans/DummyData.tsx
@@ -0,0 +1,18 @@
+export const userDummy = [
+  {
+    id: 1,
+    name: "Sam Smith",
+    email: "admin@example.com",
+    password: "zxcasdqwe",
+    avatar: "https://i.pravatar.cc/300",
+    role: "admin",
+  },
+  {
+    id: 2,
+    name: "John Doe",
+    email: "guest@example.com",
+    password: "zxcasdqwe",
+    avatar: "https://i.pravatar.cc/300",
+    role: "guest",
+  },
+];
