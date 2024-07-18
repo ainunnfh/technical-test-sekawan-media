5b7176e5836e3312873382f7b00231b99b61698c
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-07-18 13:44:25 +0700

    ⭐create overview comp

---
+++ src/components/dashboard/Overview.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+interface IProps {
+  id: number;
+  description: string;
+  date: string;
+  customerName: string;
+  priority: string;
+  status: string;
+}
+const Overview = () => {
+  const [tickets, setTickets] = useState<IProps[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/tickets");
+        const data = await response.json();
+        setTickets(data);
+      } catch (error) {
+        console.error("Error fetching tickets:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <div className="grid grid-cols-4 gap-4 w-full h-32 text-xl">
+        {[
+          "open",
+          "on hold",
+          "unresolved",
+          "overdue"
+        ].map((status) => (
+          <div
+            key={status}
+            className="flex flex-col justify-center items-center bg-slate-200 rounded-md"
+          >
+            {status}
+            <span>{tickets.filter((ticket) => ticket.status === status).length}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Overview;
+function useFetch(arg0: string): { data: any } {
+  throw new Error("Function not implemented.");
+}
