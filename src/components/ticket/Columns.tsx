75dda0ac1dad91ed5154f223840d46ae452b65bf
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-07-18 11:47:40 +0700

    ⭐new columns comp

---
+++ src/components/ticket/Columns.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { ColumnDef } from "@tanstack/react-table";
+
+interface IProps {
+  id: number;
+  description: string;
+  date: string;
+  customerName: string;
+  priority: string;
+  status: string;
+}
+const Columns: ColumnDef<IProps>[] = [
+  {
+    accessorKey: "index",
+    header: "no",
+    cell: ({ row }) => row.index + 1,
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+  },
+  {
+    accessorKey: "date",
+    header: "Date",
+  },
+  {
+    accessorKey: "customerName",
+    header: "Customer Name",
+  },
+  {
+    accessorKey: "priority",
+    header: "Priority",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+];
+
+export default Columns;
