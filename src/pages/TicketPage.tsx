a0fcf9d58ec344c1c0566897af07ebc24249c5b0
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-07-18 11:51:03 +0700

    🆙update (ticket page): usage table compt

--- src/pages/TicketPage.tsx
+++ src/pages/TicketPage.tsx
@@ -1,11 +1,34 @@
-import React from "react";
-import TableTicket from "../components/TableTicket";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
-
+import { DataTable } from "../components/ticket/DataTable";
+import Columns from "../components/ticket/Columns";
+interface IProps {
+  id: number;
+  description: string;
+  date: string;
+  customerName: string;
+  priority: string;
+  status: string;
+}
 const TicketPage = () => {
+  const [tickets, setTickets] = useState<IProps[]>([]);
+  useEffect(() => {
+    const fetchTickets = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/tickets");
+        const data = await response.json();
+        setTickets(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching tickets:", error);
+      }
+    };
+
+    fetchTickets();
+  }, []);
   return (
     <Sidebar email={""}>
-      <TableTicket />
+      <DataTable columns={Columns} data={tickets} />
     </Sidebar>
   );
 };
